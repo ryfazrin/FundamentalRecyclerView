d1c41901ac37f20dec52d59d6cffc71334462e0f
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-27 19:35:55 +0800

    Update ListHeroAdapter.kt

--- app/src/main/java/com/ryfazrin/fundamentalrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/ryfazrin/fundamentalrecyclerview/ListHeroAdapter.kt
@@ -1,4 +1,23 @@
 package com.ryfazrin.fundamentalrecyclerview
 
-class ListHeroAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
 }
