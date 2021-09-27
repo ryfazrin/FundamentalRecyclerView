ebd447efb189f451681c9463a4a78b2e052743bf
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-27 19:25:32 +0800

    Update Hero.kt
    Menambahkan implementasi parcelable

--- app/src/main/java/com/ryfazrin/fundamentalrecyclerview/Hero.kt
+++ app/src/main/java/com/ryfazrin/fundamentalrecyclerview/Hero.kt
@@ -1,7 +1,11 @@
 package com.ryfazrin.fundamentalrecyclerview
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Hero(
     var name: String,
     var description: String,
     var photo: Int
-)
+) : Parcelable
