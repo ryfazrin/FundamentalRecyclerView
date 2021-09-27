3476edd9de3533d16203ae144d15fcfe1288c1e5
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-09-27 19:59:07 +0800

    Update MainActivity.kt
    panggil data yang sudah dibuat di resource strings.xml

--- app/src/main/java/com/ryfazrin/fundamentalrecyclerview/MainActivity.kt
+++ app/src/main/java/com/ryfazrin/fundamentalrecyclerview/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ryfazrin.fundamentalrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rvHeroes: RecyclerView
+    private val list = ArrayList<Hero>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +16,27 @@ class MainActivity : AppCompatActivity() {
 
         rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
+
+        list.addAll(listHeroes)
+        showRecyclerList()
+    }
+
+    private val listHeroes: ArrayList<Hero>
+        get() {
+            val dataName = resources.getStringArray(R.array.data_name)
+            val dataDecription = resources.getStringArray(R.array.data_description)
+            val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+            val listHero = ArrayList<Hero>()
+            for (i in dataName.indices) {
+                val hero = Hero(dataName[i], dataDecription[i], dataPhoto.getResourceId(i, -1))
+                listHero.add(hero)
+            }
+            return listHero
+        }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
